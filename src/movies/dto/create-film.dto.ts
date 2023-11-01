ba7cd48f8bebfaee10c744e8101a78cd476e4ffe
Alexander Zarubin <zarubin_14@list.ru>
2023-11-01 20:05:13 +0300

    Add more slots in movie

--- src/movies/dto/create-film.dto.ts
+++ src/movies/dto/create-film.dto.ts
@@ -1,4 +1,19 @@
 export class CreateMovieDto {
     readonly tittle: string;
+
     readonly description: string;
+
+    readonly release_date: Date;
+
+    readonly duration: number;
+
+    readonly plot: string;
+
+    readonly poster_url: string;
+
+    readonly trailer_url: string;
+
+    readonly rating_total: number;
+
+    readonly rating_value: number;
 }
--- src/movies/schemas/movie.schema.ts
+++ src/movies/schemas/movie.schema.ts
@@ -10,6 +10,27 @@ export class Movie {
 
     @Prop({})
     description: string;
+
+    @Prop({})
+    release_date: Date;
+
+    @Prop({})
+    duration: number;
+
+    @Prop({})
+    plot: string;
+
+    @Prop({})
+    poster_url: string;
+
+    @Prop({})
+    trailer_url: string;
+
+    @Prop({})
+    rating_total: number;
+
+    @Prop({})
+    rating_value: number;
 }
 
 export const MovieSchema = SchemaFactory.createForClass(Movie);
